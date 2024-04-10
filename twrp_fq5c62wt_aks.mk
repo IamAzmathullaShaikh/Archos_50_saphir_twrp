d10679999e2c6efcf87e5785b9c50b37689c66a7
N00BShaikh <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-04-10 16:21:02 +0530

    Update twrp_fq5c62wt_aks.mk

--- twrp_fq5c62wt_aks.mk
+++ twrp_fq5c62wt_aks.mk
@@ -10,9 +10,6 @@ DEVICE_PATH := device/archos/fq5c62wt_aks
 # Configure base.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Configure gsi_keys.mk
-$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
 # Configure twrp
 $(call inherit-product, vendor/twrp/config/common.mk)
 
