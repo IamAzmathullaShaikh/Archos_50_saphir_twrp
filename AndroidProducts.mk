da9ded6a2d04a5481d5b0fb8c6934eb8e2a88cb3
N00BShaikh <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-04-10 15:29:50 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_fq5c62wt_aks.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_fq5c62wt_aks-user \
+    twrp_fq5c62wt_aks-userdebug \
+    twrp_fq5c62wt_aks-eng
